6b6ba912540a77a2a84cb780478a25463a0f02ea
David Goulet <dgoulet@ev0ke.net>
2016-11-09 15:46:15 -0500

    Beautify the rest of the code base
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- src/scrambler-common.h
+++ src/scrambler-common.h
@@ -1,23 +1,24 @@
 /*
-Copyright (c) 2014-2015 The scrambler-plugin authors. All rights reserved.
+ * Copyright (c) 2014-2015 The scrambler-plugin authors. All rights reserved.
+ *
+ * On 30.4.2015 - or earlier on notice - the scrambler-plugin authors will
+ * make this source code available under the terms of the GNU Affero General
+ * Public License version 3.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-On 30.4.2015 - or earlier on notice - the scrambler-plugin authors will make
-this source code available under the terms of the GNU Affero General Public
-License version 3.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #ifndef SCRAMBLER_COMMON_H
 #define SCRAMBLER_COMMON_H
 
@@ -71,26 +72,15 @@ void scrambler_initialize(void);
 
 const char *scrambler_read_line_fd(pool_t pool, int file_descriptor);
 
-const char *scrambler_hash_password(
-    const char *password,
-    const char *salt,
-    unsigned int iterations);
-
 const EVP_CIPHER *scrambler_cipher(enum packages package);
 
-void scrambler_generate_mac(
-  unsigned char *tag, unsigned int *tag_size,
-  const unsigned char *sources[], size_t source_sizes[],
-  const unsigned char *key, size_t key_size);
-
-void scrambler_unescape_pem(char *source);
-
-EVP_PKEY *scrambler_pem_read_public_key(const char *source);
-
-EVP_PKEY *scrambler_pem_read_encrypted_private_key(const char *source, const char *password);
+void scrambler_generate_mac(unsigned char *tag, unsigned int *tag_size,
+                            const unsigned char *sources[],
+                            size_t source_sizes[],
+                            const unsigned char *key, size_t key_size);
 
 void i_error_openssl(const char *function_name);
 
 void i_debug_hex(const char *prefix, const unsigned char *data, size_t size);
 
-#endif
+#endif /* SCRAMBLER_COMMON_H */
